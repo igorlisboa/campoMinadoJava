9a7ffa11d8b4ec579d036e47438ce6d431f9c78e
Igor Lisboa <igorlisboa34@gmail.com>
2018-06-18 18:08:11 -0300

    subindo arquivos java

---
+++ src/com/campo/minado/Main.java
@@ -0,0 +1,13 @@
+package com.campo.minado;
+
+public class Main {
+	public static void main(String[] args) {
+		Integer[][] campoBombas = {	{0,1,0,0,0},
+				                    {0,0,1,1,0},
+				                    {0,0,0,0,1},
+				                    {0,1,1,0,0},
+				                    {1,0,0,1,0}	};
+		
+		System.out.println(campoBombas);
+	}
+}
